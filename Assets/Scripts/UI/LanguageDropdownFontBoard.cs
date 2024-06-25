c174481ad817938ef36da9b3bd8473211da925d7
Patricia S. Gracia Artero <patryechelon@gmail.com>
2024-06-25 20:07:54 +0200

    Board selection dropdown updated to different languages
    - patrol in the process of adding audio to the ducks by clicking on them
    in the process of re-importing the japanese font

--- Assets/Scripts/UI/LanguageDropdownFontBoard.cs
+++ Assets/Scripts/UI/LanguageDropdownFontBoard.cs
@@ -1,22 +1,38 @@
-using System.Collections;
+ï»¿using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class LanguageDropdownFontBoard : MonoBehaviour, IPointerClickHandler
 {
-
-   // para el de tipo de tablero tendrás que crear otro script igual, 
-   //     pero en lugar de buscar la opción de japonés y cambiar la fuente, 
-   //     tendrás que recorrer todas las opciones y traducir cambiando la fuente
+    private string language;
 
     [SerializeField] TMP_FontAsset japaneseFont;
-
+    [SerializeField] TMP_FontAsset basicFont;
     private TMP_Dropdown dropdown;
+
+    private Dictionary<string, string> translations = new Dictionary<string, string>
+    {
+        { "English", "Classic" },
+        { "Spanish", "ClÃ¡sico" },
+        { "Japanese", "ã‚¯ãƒ©ã‚·ãƒƒã‚¯" },
+    };
 
     private void Start()
     {
         dropdown = GetComponent<TMP_Dropdown>();
+        language = PlayerPrefs.GetString("LanguageChosen");
+        UpdateLabelAndOptions();
+    }
+    private void Update()
+    {
+        string newLanguage = PlayerPrefs.GetString("LanguageChosen");
+        if (newLanguage != language)
+        {
+            language = newLanguage;
+            UpdateLabelAndOptions();
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -26,26 +42,51 @@ public class LanguageDropdownFontBoard : MonoBehaviour, IPointerClickHandler
 
     private IEnumerator WaitForOptions()
     {
-        // Espera un frame para que las opciones se generen
         yield return null;
-
-        Transform dropdownList = dropdown.transform.Find("Dropdown List");
-        if (dropdownList != null)
-        {
-            UpdateDropdownFonts(dropdownList);
-        }
+        UpdateLabelAndOptions();
     }
 
-    private void UpdateDropdownFonts(Transform optionsContainer)
-    {
-        // Recorre todas las opciones y cambia la fuente
-        foreach (Transform option in optionsContainer)
-        {
-            TMP_Text optionText = option.GetComponentInChildren<TMP_Text>();
 
-            if (optionText != null)
+    private void UpdateLabelAndOptions()
+    {
+        if (translations.ContainsKey(language))
+        {
+            // Label
+            TMP_Text dropdownLabel = dropdown.transform.Find("Label").GetComponent<TMP_Text>();
+            dropdownLabel.text = translations[language];
+            TMP_FontAsset selectedFont = basicFont;
+            if (language == "Japanese")
             {
-                optionText.font = japaneseFont;
+                dropdownLabel.font = japaneseFont;
+            }
+            else
+            {
+                dropdownLabel.font = selectedFont;
+            }
+ 
+            //Options
+            List<TMP_Dropdown.OptionData> newOptions = new List<TMP_Dropdown.OptionData>();
+            foreach (var option in dropdown.options)
+            {
+                string translatedText = translations[language];
+                TMP_Dropdown.OptionData newOption = new TMP_Dropdown.OptionData(translatedText);
+                newOptions.Add(newOption);
+            }
+            dropdown.options = newOptions;
+
+            // Refresh
+            dropdown.RefreshShownValue();
+
+            
+            // Refresh fonts on deployed dropdown
+            Transform dropdownList = dropdown.transform.Find("Dropdown List");
+            if (dropdownList != null)
+            {
+                foreach (Transform option in dropdownList)
+                {
+                    TMP_Text optionText = option.GetComponentInChildren<TMP_Text>();
+                    optionText.font = selectedFont;
+                }
             }
         }
     }
